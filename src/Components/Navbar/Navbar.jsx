700d15be2f42510245bd942b05f4cbd29e7f1302
trippinguard <mikecruzy56@gmail.com>
2024-10-08 13:43:51 +0300

    updates

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,6 +27,7 @@ const Navbar = () => {
       </nav>
       <ul className="flex justify-center w-full gap-3 bg-red-700 shadow">
         {menus.map((menu) => (
+          // eslint-disable-next-line react/jsx-key
           <li className="p-2 text-base cursor-pointer text-slate-200">
             {menu}
           </li>
