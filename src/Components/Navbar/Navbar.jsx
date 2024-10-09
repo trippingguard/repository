ce6e8e522c5b88f90c93efaafdf6b2cf1490b4b2
trippingguard <mikecruzy56@gmail.com>
2024-10-09 17:17:48 +0300

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,14 +17,13 @@ const Navbar = () => {
   return (
     <header className="mb-24">
       <nav className="bg-red-900">
-        <div className="flex p-3 gap-3 justify-between items-center w-9/12 mx-auto">
-          <img src={logo} className="size-16" alt="" />
-          <div className="text-slate-100 text-center text-2xl font-bold">
+        <div className="flex items-center justify-between w-9/12 gap-3 p-3 mx-auto">
+         <div><img src={logo} className="size-16" alt="" /></div> 
+          <div className="text-2xl font-bold text-center text-slate-100">
             <h1 className="">High Court of Tanzania</h1>
             <h2>Commercial Division</h2>
           </div>
-          <img src={logo} className="size-16" alt="" />
-          <div></div>
+          <div> <img src={logo} className="size-16" alt="" /></div>
         </div>
       </nav>
       <ul className="flex justify-center w-full gap-3 bg-red-700 shadow">
